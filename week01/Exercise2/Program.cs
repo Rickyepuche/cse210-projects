0e04552bb8fc40731da8c6c5e1611c1583ebb8db
Richard Edigin <richardedigin7988@gmail.com>
2025-07-02 21:45:29 +0100

    I completed the exercise core requirements and the stretch challenge

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+     Console.Write("What is your grade percentage? ");
+     string text = Console.ReadLine();
+     int grade = int.Parse(text);
+     string letter = "";
+     string sign = "";
+     int remainder = grade % 10;
+     if (remainder >= 7)
+     {
+        sign = "+";
+     }
+     else if (remainder < 3)
+     {
+        sign = "-";
+     }
+
+     if (grade >= 90)
+     {
+        if (remainder < 3) 
+        {
+         letter = "A";
+         sign = "-";
+        }
+        else
+        {
+         letter = "A";
+         sign = "";
+        }
+     }
+     else if (grade >= 80)
+     {
+        letter = "B";
+     }
+     else if (grade >= 70)
+     {
+        letter = "C";
+     }
+     else if (grade >= 60)
+     {
+        letter = "D";
+     }
+     else
+     {
+        letter = "F";
+        sign = "";
+     }
+
+     Console.WriteLine($"Your score is {letter}{sign}");
+     if (grade >= 70)
+     {
+        Console.Write("Congratulations, you passed this course");
+     }
+     else
+     {
+      Console.Write("Sorry, you failed this course. Try harder next time");
+     }
     }
-}
+}  
