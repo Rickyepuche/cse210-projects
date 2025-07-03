c63acff8f27925f09df6d4bd22323fb953463c47
Richard Edigin <richardedigin7988@gmail.com>
2025-07-03 15:48:05 +0100

    I completed the core requirements and the stretch challenge, the program runs exactly as instructed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("WELCOME TO GUESS MY NUMBER GAME!");
+        Console.WriteLine();
+
+        string start = "yes";
+        while (start != "no")
+        {
+            int guess = 0;
+            Random randomNumber = new Random();
+            int magicNumber = randomNumber.Next(1, 101);
+            int guessTaken = 0;
+            do
+            {
+                Console.Write("What is your guess? ");
+                string text = Console.ReadLine();
+                guess = int.Parse(text);
+                guessTaken++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!. It took you {guessTaken} guesses");
+                    Console.Write("Do you want to play again?(yes/no) ");
+                    string text2 = Console.ReadLine();
+                    start = text2.ToLower();
+                    Console.WriteLine();
+                }
+            } while (guess != magicNumber);
+        }
     }
 }
