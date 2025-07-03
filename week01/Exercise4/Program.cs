1ac67fa659701e8b4265953e9a147c72c475af45
Richard Edigin <richardedigin7988@gmail.com>
2025-07-03 21:34:45 +0100

    I completed the core requirements and the stretch challenges. Program runs fine

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //I completed the core requirements and the stretch challenges
+        List<int> numbers = new List<int>();
+
+        int userNumber = 9999;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter Number: ");
+            string text = Console.ReadLine();
+            userNumber = int.Parse(text);
+            numbers.Add(userNumber);
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max) 
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
+        int minPositiveNum = 9999;
+        foreach (int number in numbers)
+        {
+            if (number > 0) 
+            {
+                if (number < minPositiveNum)
+                {
+                    minPositiveNum = number;
+                }
+            } 
+        }
+        Console.WriteLine($"The smallest positive number is: {minPositiveNum}");
+
+        Console.WriteLine("The sorted list is:");
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
