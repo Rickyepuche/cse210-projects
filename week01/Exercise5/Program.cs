a6a5aa97992d1fa743b5e76673b1acc35f972e75
Richard Edigin <richardedigin7988@gmail.com>
2025-07-04 15:07:24 +0100

    I completed the core requirements and my program runs without errors.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName(); 
+        int number = PromptUserNumber();
+        int result = SquareNumber(number);
+        DisplayResult(name, result);
+
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string text = Console.ReadLine();
+        int userNumber = int.Parse(text);
+        return userNumber;
+    }
+    static int SquareNumber(int number)
+    {
+        int squaredTotal = number * number;
+        return squaredTotal;
+    }
+    static void DisplayResult(string name, int number) 
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
