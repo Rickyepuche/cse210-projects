486074b19925fcf19e6ec33dc86bbe99b24318aa
Richard Edigin <richardedigin7988@gmail.com>
2025-07-15 23:24:49 +0100

    I completed the coding activity, all code runs without errors

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f = new Fraction();
+        f.SetTop(1);
+        f.SetBottom(3);
+        string fraction1 = f.GetFractionString();
+        Console.WriteLine(fraction1);
+        double result = f.GetDecimalValue();
+        Console.WriteLine(result);
     }
 }
